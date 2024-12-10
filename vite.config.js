b68af6400a00b0708c00235f0e5da249c3d1a75c
VictorPDutra <161891023+VictorPDutra@users.noreply.github.com>
2024-12-10 16:40:14 -0300

    Correção da rota principal

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/app_estoque_2.0/',
+  base: '/app_estoque/',
 })
