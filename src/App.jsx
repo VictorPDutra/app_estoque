a52d25db738ae1c68666b77fbb293517a7dee3ac
VictorPDutra <161891023+VictorPDutra@users.noreply.github.com>
2025-01-17 15:29:36 -0300

    refactor: adicionar pastas para organizar componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 //App.jsx
 
 import { Routes, Route, Navigate } from "react-router-dom";
-import StockManagement from "./pages/StockManagement";
-import StockPage from "./pages/StockPage";
-import SectionPage from "./pages/SectionPage";
+import StockManagement from "./pages/home/StockManagement";
+import StockPage from "./components/stock/StockPage";
+import SectionPage from "./components/section/SectionPage";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/app_estoque" />} />{" "}
+      <Route path="/" element={<Navigate to="/app_estoque" />} />
       {/* Redireciona */}
       <Route path="/app_estoque" element={<StockManagement />} />
       <Route path="/app_estoque/stock/:id" element={<StockPage />} />
