c1d643baf1049e411a93a2785ac810c74ebb1e8e
VictorPDutra <161891023+VictorPDutra@users.noreply.github.com>
2025-01-20 17:43:12 -0300

    feat: atualizar estilos:lipstick:
    atualizado página home e navbar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import styles from "./Home.module.css";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.background_home}>
+      <div className={styles.home}>
+        <p className={styles.home_description}>
+          Bem vindo ao <span className={styles.logo}>STOCKS</span>! <br />
+          <br />
+          Esta é a melhor plataforma de controle de estoques para empresas de
+          esquadrias de alumínio.
+        </p>
+      </div>
     </div>
   );
 };
